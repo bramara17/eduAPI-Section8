b8fe077d22805bd8773c6941b8484b5a446cd6cd
bramara_ <bramara.sambada.17@gmail.com>
2022-06-29 20:25:27 +0700

    method put & delete

---
+++ cypress/e2e/api/update-user.cy.js
@@ -0,0 +1,15 @@
+describe ('Update User', () =>{
+    it('Successfully update user', () => {
+        var user = {
+            "name": "Joko",
+            "job": "Tukang Bubur"
+        }
+        
+        cy.request('PUT', 'https://reqres.in/api/users/2',user).then((response)=>{
+            expect(response.status).equal(200)
+            expect(response.body.name).to.eq(user.name)
+            
+        })
+    });
+    
+})
