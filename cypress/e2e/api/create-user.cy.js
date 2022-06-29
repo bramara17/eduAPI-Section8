878aecf338aaffb0eb3901325397b6dea7d33284
bramara_ <bramara.sambada.17@gmail.com>
2022-06-29 15:34:29 +0700

    push project api

---
+++ cypress/e2e/api/create-user.cy.js
@@ -0,0 +1,13 @@
+describe ('Create New User', () =>{
+    it('Successfully create new user', () => {
+        var user = {
+            "name": "Eduwork",
+            "job": "Education"
+        }
+        
+        cy.request('POST', 'https://reqres.in/api/users',user).then((response)=>{
+            expect(response.status).equal(201)
+        })
+    });
+    
+})
