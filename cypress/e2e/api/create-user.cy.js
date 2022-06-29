37fee7f89c7bebfa0a5849761efcea3ca7e45c90
bramara_ <bramara.sambada.17@gmail.com>
2022-06-29 15:42:06 +0700

    assertion response method post property name & job

--- cypress/e2e/api/create-user.cy.js
+++ cypress/e2e/api/create-user.cy.js
@@ -6,7 +6,9 @@ describe ('Create New User', () =>{
         }
         
         cy.request('POST', 'https://reqres.in/api/users',user).then((response)=>{
-            expect(response.status).equal(201)
+            //expect(response.status).equal(201)
+            expect(response.body).to.have.property('name', 'Eduwork')
+            expect(response.body).to.have.property('job', 'Education')
         })
     });
     
