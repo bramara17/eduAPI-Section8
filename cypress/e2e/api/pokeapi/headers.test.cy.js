4d5c8ff18bade0aba173a397a5eabf701ddbd034
bramara_ <bramara.sambada.17@gmail.com>
2022-06-29 23:02:34 +0700

    assertion menggunakan body response

--- cypress/e2e/api/pokeapi/headers.test.cy.js
+++ cypress/e2e/api/pokeapi/headers.test.cy.js
@@ -1,8 +1,11 @@
 describe ('Validate Header', () =>{
     it('Successfully validate content-type', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('content-type')
-        .should('include','application/json; charset=utf-8')
+        // cy.get('@pokemon').its('headers').its('content-type')
+        // .should('include','application/json; charset=utf-8')
+        cy.get('@pokemon').should((response) => {
+            expect(response.body).to.have.property('abilities')
+        })
     });
     
 })
